6922994ed0a47aee0288692aa0cdbac6e0467c0b
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-12-05 21:06:54 +0500

    update nltk

--- sub_app/views.py
+++ sub_app/views.py
@@ -13,24 +13,24 @@ class call_model(APIView):
     def get(self,request):
         if request.method == 'GET':
             # get sound from request
-            try:
-                  question = request.GET.get('question')
-                  question=word_tokenize(question.lower())
-                  question=" ".join([lemmatizer.lemmatize(i) for i in question])
-                  vector = PredictorConfig.vectorizer.transform([question])
-                  # predict based on vector
-                  prediction = PredictorConfig.clf.predict(vector.toarray())
-                  file_path= os.path.join(settings.MODELS, 'intents.json')
-                  with open(file_path,encoding="utf8") as file:
-                  	data = json.load(file)
-                  df=pd.DataFrame(data['intents'])
-                  df['tag']=df['tag'].str.lower()
+            #try:
+            question = request.GET.get('question')
+            question=word_tokenize(question.lower())
+            question=" ".join([lemmatizer.lemmatize(i) for i in question])
+            vector = PredictorConfig.vectorizer.transform([question])
+            # predict based on vector
+            prediction = PredictorConfig.clf.predict(vector.toarray())
+            file_path= os.path.join(settings.MODELS, 'intents.json')
+            with open(file_path,encoding="utf8") as file:
+            	data = json.load(file)
+            df=pd.DataFrame(data['intents'])
+            df['tag']=df['tag'].str.lower()
 
-                  out=df['responses'][df['tag']==prediction[0]]
-                  # build response
-                  print(out.to_list())
-                  response = {'result': out.to_list()[0][0]}
-                  # return response
-                  return JsonResponse(response)
-            except Exception as e:
-                  return JsonResponse({'result':e})
+            out=df['responses'][df['tag']==prediction[0]]
+            # build response
+            print(out.to_list())
+            response = {'result': out.to_list()[0][0]}
+            # return response
+            return JsonResponse(response)
+            #except Exception as e:
+            #      return JsonResponse({'result':e})
