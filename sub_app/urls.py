72bacd5ce093f7598ebd3fc3c9172815527aa887
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-12-11 16:56:56 +0500

    update

--- sub_app/urls.py
+++ sub_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.call_model.as_view())
+    path('api/', views.call_model.as_view())
     ]
--- sub_app/views.py
+++ sub_app/views.py
@@ -37,4 +37,4 @@ class call_model(APIView):
                   # return response
                   return JsonResponse(response)
             except :
-                  return JsonResponse({'result':"https://autiskabot.herokuapp.com/?question=PASS_HERE"})
+                  return JsonResponse({'result':"Please ask a question."})
