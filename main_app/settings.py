0b003b73b18af5208e3d4b20d4d2e51dd078f861
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2021-02-06 18:22:25 +0500

    Update settings.py

--- main_app/settings.py
+++ main_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f6%15ynbggvin-2a2jfhl==1s62io^uo8fmjbpt=yka8326e_i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
